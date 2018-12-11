3a8da6b38570110aa211e27159ee86afaf564395
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-12-11 07:12:15 +0100

    Build Todo component [basic]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebaseClient from './services/firebase';
+
+const TodoRecord = (todo, id) => (
+  <tr className="table" key={id}>
+    <td>{todo.item}</td>
+  </tr>
+);
 
 class App extends Component {
+  state = {
+    todos: [],
+    todo: '',
+  }
+
+  handleChange = e => {
+    const { target: { value } } = e;
+
+    this.setState({
+      todo: value
+    })
+  }
+
+  handleSubmit = async todo => {
+    await firebaseClient.addTodo(todo)
+  }
+
+  componentDidMount() {
+    firebaseClient.setTodoListener((querySnapshot) => {
+      const todos = [];
+      querySnapshot.forEach((todo) => {
+        todos.push(todo.data());
+      });
+      this.setState({
+        todos
+      });
+    });
+  }
   render() {
+    const { todos, todo } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +54,19 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        <input type="text" value={todo} onChange={this.handleChange} />
+        <button onClick={() => this.handleSubmit(todo)}>Add Todo</button>
+        <table>
+          <thead>
+            <tr>
+              <th> Todos </th>
+            </tr>
+          </thead>
+          <tbody>
+            { todos.map((todo, id) => (TodoRecord(todo, id))) }
+          </tbody>
+        </table>
       </div>
     );
   }
