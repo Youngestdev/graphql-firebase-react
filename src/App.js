3c6dbb106c82b6b2275c1d5555d212f48027fc74
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-12-11 07:16:02 +0100

    Removed unused code.

--- src/App.js
+++ src/App.js
@@ -41,20 +41,6 @@ class App extends Component {
     const { todos, todo } = this.state;
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
         <input type="text" value={todo} onChange={this.handleChange} />
         <button onClick={() => this.handleSubmit(todo)}>Add Todo</button>
         <table>
